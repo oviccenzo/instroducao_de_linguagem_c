b37cd96f7205f14eb3dfbb007e0c64f45795d1cb
viccenzo <viccenzo.oliveira@gmail.com>
2025-12-15 14:51:47 -0300

    Update 1.6-CriarUmProgramaQueParaTestarAsFunções.cpp

--- capitulo-1-instroducao-a-linguagem-c/1.7.3-EscreveUmProgramaQueLeDoisNumeros.cpp .cpp
+++ capitulo-1-instroducao-a-linguagem-c/1.7.3-EscreveUmProgramaQueLeDoisNumeros.cpp .cpp
@@ -1,3 +1,23 @@
 //
 // Created by Robert L Resende on 15/12/25.
 //
+#include "cstdio"
+
+int main(){
+	int a,b,c,resutaldo;
+
+	printf("Digite um numero interio: \n");
+	scanf("%d",&a);
+
+	printf("Digite outro numero interio: \n");
+	scanf("%d",&b);
+
+//    printf("Digite outro numero inteiro: \n");
+//    scanf("%d", &c);
+
+	resutaldo = a % b;
+
+	printf("O resto da divisão do primeiro número pelo segundo é = %d\n",resutaldo);
+
+	return 0;
+}

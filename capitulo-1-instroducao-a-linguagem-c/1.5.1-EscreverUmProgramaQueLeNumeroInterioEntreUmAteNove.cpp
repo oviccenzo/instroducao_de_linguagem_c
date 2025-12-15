c43dacbf0ee289eea9e34f6e75bbc2a832d4361f
viccenzo <viccenzo.oliveira@gmail.com>
2025-12-15 13:30:39 -0300

    Initial commit

--- capitulo-1-instroducao-a-linguagem-c/1.5.1-EscreverUmProgramaQueLeNumeroInterioEntreUmAteNove.cpp
+++ capitulo-1-instroducao-a-linguagem-c/1.5.1-EscreverUmProgramaQueLeNumeroInterioEntreUmAteNove.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Robert L Resende on 15/12/25.
 //
+#include "cstdio"
+
+int main(){
+
+	int x;
+	printf("Digite um numero inteiro entre 0 e 9: \n");
+	scanf("%d", &x);
+
+	printf("x = %d\n",x);
+}
